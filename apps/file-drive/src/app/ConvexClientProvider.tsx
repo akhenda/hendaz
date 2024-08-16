f81df16e7a987bc2917a358b167eab2b7b7384bc
akhenda <akhenda@gmail.com>
2024-08-16 18:41:10 +0300

    chore(file-drive): :hammer: clerk auth setup

--- apps/file-drive/src/app/ConvexClientProvider.tsx
+++ apps/file-drive/src/app/ConvexClientProvider.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import { ReactNode } from 'react';
+import { ClerkProvider, useAuth } from '@clerk/nextjs';
 import config from '@hendaz/constants/web';
-import { ConvexProvider, ConvexReactClient } from 'convex/react';
+import { ConvexReactClient } from 'convex/react';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
 
 const convex = new ConvexReactClient(config.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ClerkProvider publishableKey={config.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
---
+++ libs/backend/convex/auth.config.ts
@@ -0,0 +1,5 @@
+const config = {
+  providers: [{ applicationID: 'convex', domain: 'https://fleet-kid-30.clerk.accounts.dev' }],
+};
+
+export default config;
