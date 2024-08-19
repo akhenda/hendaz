6b11f0572a02dd654c46af6bb6cae2fe85c7acff
akhenda <akhenda@gmail.com>
2024-08-19 09:30:59 +0300

    refactor(file-drive): :recycle: test clerk auth

--- apps/file-drive/src/app/page.tsx
+++ apps/file-drive/src/app/page.tsx
@@ -1,14 +1,24 @@
+'use client';
+
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/nextjs';
+
 import { Button } from '@hendaz/ds/ui-kit';
 
 export default function Index() {
   return (
     <div className="bg-slate-600 h-screen w-full flex items-center justify-center flex-col">
       <h1 className="text-white text-6xl">Hello world</h1>
-      <br />
-      <div className="flex gap-2">
-        <Button>Button</Button>
-        <Button variant="destructive">Button</Button>
-        <Button variant="outline">Button</Button>
+      <div className="flex gap-2 mt-4">
+        <SignedIn>
+          <SignOutButton>
+            <Button variant="secondary">Sign Out</Button>
+          </SignOutButton>
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
       </div>
     </div>
   );
