e49cef2e5ee7849fa8472402f8122a388b4ccca9
akhenda <akhenda@gmail.com>
2024-08-19 09:21:54 +0300

    feat(ds-ui-kit): :sparkles: generate shadcn button

--- apps/file-drive/src/app/ConvexClientProvider.tsx
+++ apps/file-drive/src/app/ConvexClientProvider.tsx
@@ -2,9 +2,10 @@
 
 import { ReactNode } from 'react';
 import { ClerkProvider, useAuth } from '@clerk/nextjs';
-import config from '@hendaz/constants/web';
 import { ConvexReactClient } from 'convex/react';
 import { ConvexProviderWithClerk } from 'convex/react-clerk';
+
+import config from '@hendaz/constants/web';
 
 const convex = new ConvexReactClient(config.env.NEXT_PUBLIC_CONVEX_URL!);
 
