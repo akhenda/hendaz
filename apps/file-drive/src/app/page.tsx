d26aed79a00be3a1999ea2d357fbb587a7906b38
akhenda <akhenda@gmail.com>
2024-08-19 10:01:26 +0300

    feat(backend): :sparkles: test out convex

--- apps/file-drive/src/app/page.tsx
+++ apps/file-drive/src/app/page.tsx
@@ -1,14 +1,19 @@
 'use client';
 
 import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/nextjs';
+import { useMutation, useQuery } from 'convex/react';
 
+import { api } from '@hendaz/backend/convex/_generated/api';
 import { Button } from '@hendaz/ds/ui-kit';
 
 export default function Index() {
+  const files = useQuery(api.files.getFiles);
+  const createFile = useMutation(api.files.createFile);
+
   return (
     <div className="bg-slate-600 h-screen w-full flex items-center justify-center flex-col">
       <h1 className="text-white text-6xl">Hello world</h1>
-      <div className="flex gap-2 mt-4">
+      <div className="flex gap-2 m-4">
         <SignedIn>
           <SignOutButton>
             <Button variant="secondary">Sign Out</Button>
@@ -20,6 +25,23 @@ export default function Index() {
           </SignInButton>
         </SignedOut>
       </div>
+
+      <div className="m-4 flex flex-col gap-2">
+        {files?.map((file) => (
+          <div key={file._id} className="bg-slate-300 p-2 rounded-md">
+            {file.name}
+          </div>
+        ))}
+      </div>
+
+      <Button
+        className="mt-4"
+        onClick={() => {
+          createFile({ name: 'Hello, World' });
+        }}
+      >
+        Click Me
+      </Button>
     </div>
   );
 }
--- libs/backend/convex/_generated/api.d.ts
+++ libs/backend/convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as files from "../files.js";
 import type * as utils from "../utils.js";
 
 /**
@@ -25,6 +26,7 @@ import type * as utils from "../utils.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  files: typeof files;
   utils: typeof utils;
 }>;
 export declare const api: FilterApi<
